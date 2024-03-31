38497490ab84adb2073e8452b170c3b54b64eb66
Sifat Ikram <sifatikram@gmail.com>
2024-04-01 00:45:35 +0600

    server side completed

--- index.js
+++ index.js
@@ -5,135 +5,140 @@ const cors = require("cors");
 const port = process.env.PORT || 4321;
 
 // middleware
-app.use(cors({
-    origin: ['http://localhost:5173'],
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
     credentials: true,
-}));
+  })
+);
 app.use(express.json());
 
-
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jrqljyn.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-    serverApi: {
-        version: ServerApiVersion.v1,
-        strict: true,
-        deprecationErrors: true,
-    }
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
 });
 
 async function run() {
-    try {
-        // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
 
-        const taskCollection = client.db("task-master").collection("task");
+    const taskCollection = client.db("task-master").collection("task");
+    const userCollection = client.db("task-master").collection("user");
+    const bookingCollection = client.db("task-master").collection("booking");
+    const runningCollection = client.db("task-master").collection("running");
+    const completedCollection = client.db("task-master").collection("completed");
 
-        // user api
-        app.post("/user", async (req, res) => {
-            const user = req.body;
-            const result = await userCollection.insertOne(user);
-            res.send(result);
-        })
-        app.get("/user", verifyToken, verifyAdmin, async (req, res) => {
-            // console.log(req.headers);
-            const result = await userCollection.find().toArray();
-            res.send(result);
-        })
+    // user api
+    app.post("/user", async (req, res) => {
+      const user = req.body;
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
+    app.get("/user", async (req, res) => {
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    });
 
-        // tour api
-        app.get("/task", async (req, res) => {
-            const result = await taskCollection.find().toArray();
-            res.send(result);
-        })
+    // task api
+    app.get("/task", async (req, res) => {
+      const result = await taskCollection.find().toArray();
+      res.send(result);
+    });
 
-        app.post("/task", async (req, res) => {
-            const task = req.body;
-            const result = await taskCollection.insertOne(task);
-            res.send(result);
-        })
-        app.delete("/task/:id", async (req, res) => {
-            const id = req.params.id;
-            const query = { _id: new ObjectId(id) };
-            const result = await taskCollection.deleteOne(query);
-            res.send(result);
-        })
+    app.post("/task", async (req, res) => {
+      const task = req.body;
+      const result = await taskCollection.insertOne(task);
+      res.send(result);
+    });
+    app.delete("/task/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await taskCollection.deleteOne(query);
+      res.send(result);
+    });
 
-        app.patch('/task/:id', async (req, res) => {
-            const item = req.body;
-            const id = req.params.id;
-            const filter = { _id: new ObjectId(id) };
-            const options = { upsert: true };
-            const updatedDoc = {
-                $set: {
-                    title: item.title,
-                    division: item.division,
-                    price: parseFloat(item.price),
-                    deadline: item.deadline,
-                    places: item.places,
-                    transportation: item.transportation,
-                    included_item: item.included_item,
-                    description: item.description,
-                    image: item.image
-                }
-            }
+    //   bookings api
+    app.post("/bookings", async (req, res) => {
+      const bookingItem = req.body;
+      const result = await bookingCollection.insertOne(bookingItem);
+      res.send(result);
+    });
+    app.get("/bookings", async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+      const result = await bookingCollection.find(query).toArray();
+      res.send(result);
+    });
+    app.delete("/bookings/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await bookingCollection.deleteOne(query);
+      res.send(result);
+    });
 
-            const result = await taskCollection.updateOne(filter, updatedDoc, options);
-            res.send(result);
-        })
+    //   running api
+    app.post("/running", async (req, res) => {
+      const runningItem = req.body;
+      const result = await runningCollection.insertOne(runningItem);
+      res.send(result);
+    });
+    app.get("/running", async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+      const result = await runningCollection.find(query).toArray();
+      res.send(result);
+    });
+    app.delete("/running/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await runningCollection.deleteOne(query);
+      res.send(result);
+    });
 
-        //   bookings api
-        app.post('/bookings', async (req, res) => {
-            const bookingItem = req.body;
-            const result = await bookingCollection.insertOne(bookingItem);
-            res.send(result);
-        })
-        app.get('/bookings', async (req, res) => {
-            const email = req.query.email;
-            const admin = req.query.admin;
-            let query = {};
 
-            if (email) {
-                query.email = email;
-            } else if (admin) {
-                query.admin = admin;
-            }
-            const result = await bookingCollection.find(query).toArray();
-            res.send(result);
-        })
-        app.delete("/bookings/:id", async (req, res) => {
-            const id = req.params.id;
-            const query = { _id: new ObjectId(id) };
-            const result = await bookingCollection.deleteOne(query);
-            res.send(result);
-        })
-        app.delete("/bookings/admin/:id", async (req, res) => {
-            const id = req.params.id;
-            const query = { eventId: new ObjectId(id) };
-            const result = await bookingCollection.deleteOne(query);
-            res.send(result);
-        })
+    //   completed api
+    app.post("/completed", async (req, res) => {
+      const completedItem = req.body;
+      const result = await completedCollection.insertOne(completedItem);
+      res.send(result);
+    });
+    app.get("/completed", async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+      const result = await completedCollection.find(query).toArray();
+      res.send(result);
+    });
+    app.delete("/completed/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await completedCollection.deleteOne(query);
+      res.send(result);
+    });
 
-        
-        // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
-    } finally {
-        // Ensures that the client will close when you finish/error
-        // await client.close();
-    }
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
 }
 run().catch(console.dir);
 
-
-
-
-app.get('/', (req, res) => {
-    res.send("task master is mastering")
-})
+app.get("/", (req, res) => {
+  res.send("task master is mastering");
+});
 
 app.listen(port, () => {
-    console.log(`task master is mastering through port ${port}`);
-})
+  console.log(`task master is mastering through port ${port}`);
+});
