06911823ab362e00606e08c2620a2845c2499a92
Sifat Ikram <sifatikram@gmail.com>
2024-04-01 10:18:26 +0600

    server side updated

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
+var jwt = require("jsonwebtoken");
 const cors = require("cors");
 const port = process.env.PORT || 4321;
 
@@ -12,6 +13,21 @@ app.use(
   })
 );
 app.use(express.json());
+
+const verifyToken = (req, res, next) => {
+  // console.log("inside middleware", req.headers.authorization);
+  if (!req.headers.authorization) {
+    return res.status(401).send({ message: "Access forbidden" });
+  }
+  const token = req.headers.authorization.split(" ")[1];
+  jwt.verify(token, process.env.SECRET_TOKEN, (error, decoded) => {
+    if (error) {
+      return res.status(401).send({ message: "Access forbidden" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+};
 
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jrqljyn.mongodb.net/?retryWrites=true&w=majority`;
@@ -34,11 +50,27 @@ async function run() {
     const userCollection = client.db("task-master").collection("user");
     const bookingCollection = client.db("task-master").collection("booking");
     const runningCollection = client.db("task-master").collection("running");
-    const completedCollection = client.db("task-master").collection("completed");
+    const completedCollection = client
+      .db("task-master")
+      .collection("completed");
+
+    // jwt api
+    app.post("/jwt", (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.SECRET_TOKEN, {
+        expiresIn: "1h",
+      });
+      res.send({ token });
+    });
 
     // user api
     app.post("/user", async (req, res) => {
       const user = req.body;
+      const query = { email: user.email };
+      const ifExist = await userCollection.findOne(query);
+      if (ifExist) {
+        return res.send({ message: "user already exist", insertedId: null });
+      }
       const result = await userCollection.insertOne(user);
       res.send(result);
     });
@@ -102,7 +134,6 @@ async function run() {
       const result = await runningCollection.deleteOne(query);
       res.send(result);
     });
-
 
     //   completed api
     app.post("/completed", async (req, res) => {
